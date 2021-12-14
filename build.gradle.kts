d6c64291340f2b78a794a5a5a56168d701c0e0cf
Tefek <31473117+493msi@users.noreply.github.com>
2021-12-14 22:41:55 +0100

    Updated log4j to version 2.16

--- build.gradle.kts
+++ build.gradle.kts
@@ -123,9 +123,9 @@ dependencies {
 
     implementation("org.slf4j", "slf4j-api", "1.8.0-beta4")
     implementation("org.slf4j", "slf4j-simple", "1.8.0-beta4")
-    implementation("org.apache.logging.log4j", "log4j-core", "2.15.0")
-    implementation("org.apache.logging.log4j", "log4j-api", "2.15.0")
-    implementation("org.apache.logging.log4j", "log4j-slf4j-impl", "2.15.0")
+    implementation("org.apache.logging.log4j", "log4j-core", "2.16.0")
+    implementation("org.apache.logging.log4j", "log4j-api", "2.16.0")
+    implementation("org.apache.logging.log4j", "log4j-slf4j-impl", "2.16.0")
 
     implementation("org.apache.commons", "commons-lang3", "3.11")
     implementation("org.apache.commons", "commons-text", "1.9")
